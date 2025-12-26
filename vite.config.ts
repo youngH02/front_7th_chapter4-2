0011da0cc71f1af3c99fde339825616dbbe1c40f
JiYoung Park <jyoungp02@gmail.com>
2025-12-26 09:03:16 +0900

    배포 base 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default mergeConfig(
   defineConfig({
+    base: '/front_7th_chapter4-2/', 
     plugins: [react()],
   }),
   defineTestConfig({
